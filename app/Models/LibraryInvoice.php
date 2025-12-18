bb9ad902cde22bc8fec410f1a77c032af96ee62b
John The Bryan <johnbryanjavellana@gmail.com>
2025-12-18 13:35:06 +0800

    12-16-2025

--- app/Models/LibraryInvoice.php
+++ app/Models/LibraryInvoice.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class LibraryInvoice extends Model
 {
     use HasFactory;
+
+    public function payee () {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function bookRes () {
+        return $this->belongsTo(BookRes::class, 'book_res_id', 'id');
+    }
+
+    public function selectedBooks () {
+        return $this->hasMany(LISelectedBook::class);
+    }
 }
