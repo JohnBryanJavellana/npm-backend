d75391f227fe7728f9775b21499dc49b8d3967f1
John The Bryan <johnbryanjavellana@gmail.com>
2025-12-20 14:50:08 +0800

    Fixing Jobs

--- app/Jobs/SendingEmail.php
+++ app/Jobs/SendingEmail.php
@@ -7,29 +7,24 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Mail\Mailable;
 use App\Models\User;
 
 class SendingEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-
     protected $tries = 3;
     protected $timeout = 3600;
-    /**
-     * Create a new job instance.
-     */
-    public function __construct(public User $user,public $mailable) {}
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
-    {
+    public function __construct(
+        protected User $user,
+        protected Mailable $mailable
+    ) {}
+
+    public function handle() {
         try {
             \Mail::to($this->user->email)->send($this->mailable);
-            \Log::info("sent mail job handle", [$this->user->email]);
-
         } catch (\Throwable $th) {
             \Log::error("error mail job handle", [$th]);
         }
