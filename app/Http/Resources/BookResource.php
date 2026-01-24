a95a796c6e4ec0233ac107edea2cd270b96e32b7
Allen Alfred Beato <allenalfred.beato4@gmail.com>
2026-01-24 14:42:48 +0800

    Library Validation

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -30,10 +30,11 @@ class BookResource extends JsonResource
             "type" => $this->catalog?->type,
             'file_location' => $this->catalog?->file_location,
             'call_number' => $this->catalog?->call_number,
-            'publication_year' => $this->catalog?->publication_year,
+            'publication_year' => $this->catalog?->publication_year,    
             'entry' => $this->catalog?->genre?->name,
             'count' => $this->copies_count,
-            'flag' => ($this->carts_count > 0|| $this->has_data_count > 0) ? true : false,
+            "borrrow_count" =>  $this->has_data_count,
+            'flag' => ($this->carts_count > 0||$this->has_data_count > 0) ? true : false,
             'recommendation' => $this->related->map(function($course) {
                 return $course->training?->module?->name;
             }),
