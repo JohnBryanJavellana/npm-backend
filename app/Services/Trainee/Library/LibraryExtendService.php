5b93f9be38d14d2565f6fed29ae72413588805b2
Allen Alfred Beato <allenalfred.beato4@gmail.com>
2026-01-24 11:17:08 +0800

    library_extenstion_email

--- app/Services/Trainee/Library/LibraryExtendService.php
+++ app/Services/Trainee/Library/LibraryExtendService.php
@@ -2,26 +2,48 @@
 
 namespace App\Services\Trainee\Library;
 
+use App\Enums\RequestStatus;
 use App\Models\BookExtensionRequest;
+use App\Models\BookReservation;
 use Illuminate\Support\Facades\DB;
 
 class LibraryExtendService {
     public function __construct(
         private BookExtensionRequest $bookExtensionRequest,
+        protected BookReservation $bookReservationModel,
+        protected LibraryExtraService $libraryExtraService,
     )
     {}
 
-    public function createExtendRequest()
-    {
-        DB::transaction(function() {
-            $this->prepareData();
 
+    private function prepareData(){
+        return;
+    }
+    public function createExtendRequest($validated)
+    {
+        DB::transaction(function($validated) {
+            // $this->prepareData();
+
+            $book_ids = collect($validated["data"])->pluck("book_res_id");
+
+            $records = $this->bookReservationModel->query()
+            ->select("id", "status")
+            ->forStatus([RequestStatus::RECEIVED->value])
+            ->whereRelation("bookRes", "user_id", "=", $validated["user_id"])
+            ->whereIn("id",$book_ids)
+            ->lockForUpdate()
+            ->get();
+
+            $this->libraryExtraService->storeExtraService($validated, $validated["user_id"], "EXTEND");
+            
+            foreach($records as $record) {
+                $record->status = RequestStatus::EXTENDING->value;
+                $record->save();
+            }
         });
     }
 
-    private function prepareData(){
 
-    }
     
     
 }
