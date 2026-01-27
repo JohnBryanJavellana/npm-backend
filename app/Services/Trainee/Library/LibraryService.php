8a5c511f552a51b24765fdfbfc5e333ea345a2d8
Allen Alfred Beato <allenalfred.beato4@gmail.com>
2026-01-27 11:28:13 +0800

    new

--- app/Services/Trainee/Library/LibraryService.php
+++ app/Services/Trainee/Library/LibraryService.php
@@ -105,6 +105,7 @@ class LibraryService {
                     "book_id" => $book["book_id"],
                     "type" => $book["copy_type"],
                     "from_date" => Carbon::parse($validated["from"]),
+                    //new
                     "to_date" => Carbon::parse($validated["from"])->addDays(6),
                 ]);
             }
