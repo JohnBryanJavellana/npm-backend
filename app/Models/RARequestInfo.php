c46cfa22061953b583109517578c78ca15ad421f
John The Bryan <johnbryanjavellana@gmail.com>
2026-02-05 16:31:39 +0800

    123

--- app/Models/RARequestInfo.php
+++ app/Models/RARequestInfo.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class RARequestInfo extends Model
 {
     use HasFactory;
+
+    public function requestor() {
+        return $this->hasOne(User::class);
+    }
 }
