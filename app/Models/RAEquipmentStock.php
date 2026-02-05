34231a7d5838f0ae05b17c37bd29fbb74aec59cd
Allen Alfred Beato <allenalfred.beato4@gmail.com>
2026-02-05 16:49:23 +0800

    scopes model

--- app/Models/RAEquipmentStock.php
+++ app/Models/RAEquipmentStock.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RAEquipmentStock extends Model
 {
     use HasFactory;
+
+    protected $guarded = ["id"];
+
+
+    /**
+     * Scopes
+     **/
+
+    public function scopeAvailable(Builder $query)
+    {
+        return $query->where("availability_status", "AVAILABLE");
+    }
+
+    public function scopeOkayCondition(Builder $query)
+    {
+        return $query->whereIn("condition_status", ["GOOD CONDITION", "DAMAGED"]);
+    }
 }
