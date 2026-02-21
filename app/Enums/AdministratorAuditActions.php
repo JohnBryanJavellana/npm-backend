f3a41b8b33c338dc88da57773ca8d1aee8833c22
John The Bryan <johnbryanjavellana@gmail.com>
2026-02-21 16:55:07 +0800

    123

--- app/Enums/AdministratorAuditActions.php
+++ app/Enums/AdministratorAuditActions.php
@@ -94,7 +94,6 @@ enum AdministratorAuditActions: string {
     case ENROLLMENTCTRL_UPDATED_ENROLLMENTTRAININGFEE = "Updated a training fee.";
     case ENROLLMENTCTRL_REMOVED_ENROLLMENTTRAININGFEE = "Removed a training fee.";
 
-    case ENROLLMENTCTRL_MOVED_TRAINEE = 'Trainees move operation completed.';
     case ENROLLMENTCTRL_MOVED_TRAINEE = 'Administrator moved trainee(s) to a new schedule.';
 
     /**
