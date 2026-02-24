a960878bd34c9d480c39c8fdf0a3c6811cff39a2
John The Bryan <johnbryanjavellana@gmail.com>
2026-02-24 09:15:03 +0800

    123

--- routes/admin-api.php
+++ routes/admin-api.php
@@ -116,8 +116,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
             Route::post('update_dormitory_room', [DormitoryController::class, 'update_dormitory_room']);
             Route::post('create-walk-in-request/get_available_dorms', [DormitoryController::class, 'get_available_dorms']);
             Route::post('create-walk-in-request/get_available_rooms', [DormitoryController::class, 'get_available_rooms']);
-            Route::post('create-walk-in-request/get_available_supplies', [DormitoryController::class, 'get_available_supplies']);
 
+            Route::post('requests/get_available_supplies', [DormitoryController::class, 'get_available_supplies']);
             Route::post('requests/update_dormitory_room_request', [DormitoryController::class, 'update_dormitory_room_request']);
 
             Route::post('get_all_requests', [DormitoryController::class, 'get_all_requests']);
