b3ad94544f4dffa59736245c28206f90c6e8a09f
John The Bryan <johnbryanjavellana@gmail.com>
2026-02-26 17:13:43 +0800

    123

--- app/Utils/GenerateQR.php
+++ app/Utils/GenerateQR.php
@@ -15,21 +15,29 @@ class GenerateQR {
     public function generate($filename, $content, $qrText = null, $path) {
         $qr_path = public_path($path . $filename);
 
-        $qrBinary = QrCode::format('png')
+        QrCode::format('png')
             ->size(500)
             ->style('round')
             ->margin(1)
             ->backgroundColor(255, 255, 255)
             ->merge(public_path('system-images/62334fcadd0d9e6d0a152aca.png'), 0.19, true)
-            ->generate($content);
+            ->generate($content, $qr_path);
 
-        $img = $this->manager->read($qrBinary);
+        $img = $this->manager->read($qr_path);
 
-        $img->resizeCanvas(500, 560, 'ffffff', 'top');
+        $img->resizeCanvas(500, 560, 'ffffff', false);
 
         if(!is_null($qrText)) {
-            $img->text($qrText, 250, 540, function($font) {
-                $font->file(public_path('fonts/Roboto/Roboto.ttf'));
+            // Ensure the path is absolute and real
+            $fontPath = public_path('fonts/Roboto/Roboto.ttf');
+
+            if (!file_exists($fontPath)) {
+                throw new \Exception("Font file not found at: " . $fontPath);
+            }
+
+            $img->text((string)$qrText, 250, 540, function($font) use ($fontPath) {
+                // Use the absolute path variable
+                $font->file($fontPath);
                 $font->size(25);
                 $font->color('#000000');
                 $font->align('center');
