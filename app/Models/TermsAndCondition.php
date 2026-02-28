aa9061345e5122a65d45b10ff8b61e474283cb2b
John The Bryan <johnbryanjavellana@gmail.com>
2026-02-28 16:25:27 +0800

    123

--- app/Models/TermsAndCondition.php
+++ app/Models/TermsAndCondition.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class TermsAndCondition extends Model
 {
     use HasFactory;
+
+    public function editor() {
+        return $this->hasOne(User::class, 'updated_by', 'id');
+    }
+
+    public function userSignedRAInvoices() {
+        return $this->hasMany(RAInvoices::class);
+    }
+
+    public function userSignedEnrollmentInvoice() {
+        return $this->hasMany(EnrollmentInvoice::class);
+    }
+
+    public function userSignedLibraryInvoice() {
+        return $this->hasMany(LibraryInvoice::class);
+    }
+
+    public function userSignedDormitoryInvoice() {
+        return $this->hasMany(DormitoryInvoice::class);
+    }
+
+    public function userSignedBookRes() {
+        return $this->hasMany(BookRes::class);
+    }
+
+    public function userSignedDormitoryTenant() {
+        return $this->hasMany(DormitoryTenant::class);
+    }
+
+    public function userSignedEnrolledCourse() {
+        return $this->hasMany(EnrolledCourse::class);
+    }
+
+    public function userSignedRARequestInfo() {
+        return $this->hasMany(RARequestInfo::class);
+    }
 }
