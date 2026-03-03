da69ac872cdf69b1688014ca5abddba3155ae04b
isaac <isaac.rita@evsu.edu.ph>
2026-03-03 08:30:33 +0800

    eirkbf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            BookSeeder::class,
             EnrollmentAdminAccountSeeder::class,
             SchoolCourseSeeder::class,
             LicenseRankSeeder::class
