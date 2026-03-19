e6afde58ca2c489c28bb12795278cd88a865705a
John The Bryan <johnbryanjavellana@gmail.com>
2026-03-19 12:25:13 +0800

    123

--- database/migrations/2025_10_04_005104_create_dormitory_rooms_table.php
+++ database/migrations/2025_10_04_005104_create_dormitory_rooms_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->foreignIdFor(Dormitory::class)->constrained()->cascadeOnDelete();
             $table->string('room_name', 255);
             $table->integer('room_slot');
-            $table->integer('room_available_slot');
             $table->enum('room_status', ['AVAILABLE', 'OCCUPIED', 'RESERVED', 'UNAVAILABLE'])->default('AVAILABLE');
             $table->enum('is_air_conditioned',["YES", "NO"]);
             $table->longText('remarks')->nullable();
