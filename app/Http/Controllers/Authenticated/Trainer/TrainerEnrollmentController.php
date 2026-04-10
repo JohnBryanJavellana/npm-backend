96affac478cb211c6985d65856a6d8074f0b0fde
isaac rita <isaac.rita@evsu.edu.ph>
2026-04-10 17:05:37 +0800

    last commit of the day

--- app/Http/Controllers/Authenticated/Trainer/TrainerEnrollmentController.php
+++ app/Http/Controllers/Authenticated/Trainer/TrainerEnrollmentController.php
@@ -110,7 +110,7 @@ class TrainerEnrollmentController extends Controller
 
 
 
-    // ! ayaw pag labot, Izack ini nag ayad, para lista hand mga attempts
+    // ! ayaw pag labot, Izack ini nag ayad, para lista hand mga attemptss
     public function TraineeAssessmentDetails(Request $request)
     {
         return TransactionUtil::transact(null, [], function () use ($request) {
